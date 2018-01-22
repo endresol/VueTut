260e5d44d466d49a7ca68d3c34532cbf42aec4f3
Endre Solem <esolem@gmail.com>
2018-01-23 00:19:01 +0100

    finalizing the jokster app tut

--- ClientApp/store/actions.js
+++ ClientApp/store/actions.js
@@ -1,15 +1,29 @@
 import * as types from './mutation-types';
 
-const jokes_10_url = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten';
+const jokes_url = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev';
 
 export const initJokes = ({commit}) => {
-  fetch(jokes_10_url, {
+  fetch(`${jokes_url}/random_ten`, {
     Method: 'GET',
   })
   .then(response => response.json())
   .then(json => commit(types.INIT_JOKES, json));
 };
 
-export const setCounter = ({commit}, obj) => {
-    commit(types.MAIN_SET_COUNTER, obj)
+export const addJoke = ({commit}, obj) => {
+  fetch(`${jokes_url}/random_joke`, {
+    Method: 'GET',
+  })
+  .then(response => response.json())
+  .then(json => commit(types.ADD_JOKE, json));
 };
+
+export const removeJoke = ({commit}, index) => {
+  commit(types.REMOVE_JOKE, index);
+}
+
+export const setCounter = ({commit}, obj) => {
+  commit(types.MAIN_SET_COUNTER, obj)
+};
+
+
